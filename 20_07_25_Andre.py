d98d4ef03411f89df255d73c1b9488775993a729
tr3b <andre@stringkeeper.com>
2020-07-27 16:08:50 -0700

    Andre Doumad

--- 20_07_25_Andre.py
+++ 20_07_25_Andre.py
@@ -3,7 +3,8 @@ Andre Doumad
 
 This problem was asked by Jane Street.
 
-cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. 
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) 
+returns the first and last element of that pair. 
 For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
 
 Given this implementation of cons:
@@ -23,22 +24,15 @@ def cons(a, b):
 
 #  car(cons(3, 4)) returns 3
 def car(x):
-    print(x)
     print('car a: ' + str(x.__closure__[0].cell_contents))
     def first(a,b):
-        print('first a: ' + str(a))
-        print('first b: ' + str(b))
         return a
     return x(first)
 print('result of car(cons(3, 4)): ' + str(car(cons(3, 4))))
 
 # cdr(cons(3, 4)) returns 4
 def cdr(x):
-    print(x)
-    print('cdr b: ' + str(x.__closure__[1].cell_contents))
     def last(a,b):
-        print('last a: ' + str(a))
-        print('last b: ' + str(b))
         return b
     return x(last)
 print('result of cdr(cons(3, 4)): ' + str(cdr(cons(3, 4))))
