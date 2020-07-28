d4131f0207f2a93570ffb097eb4ac23b7034f6b8
tr3b <andre@stringkeeper.com>
2020-07-28 10:53:36 -0700

    Andre Doumad

--- 20_07_27_Andre.py
+++ 20_07_27_Andre.py
@@ -1,10 +1,38 @@
 '''
 This problem was asked by Facebook.
 
-Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, 
+count the number of ways it can be decoded.
 
-For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+For example, the message '111' would give 3, since it 
+could be decoded as 'aaa', 'ka', and 'ak'.
 
-You can assume that the messages are decodable. For example, '001' is not allowed.
-
+You can assume that the messages are decodable. 
+For example, '001' is not allowed.
 '''
+
+# write a method to map the letters of the alphabet to to 1-26
+
+from collections import defaultdict
+
+def num_encodings(s):
+    # On lookup, this hashmap returns a default value of 0 if the key doesn't exist
+    # cache[i] gives us # of ways to encode the substring s[i:]
+    cache = defaultdict(int)
+    cache[len(s)] = 1 # Empty string is 1 valid encoding
+
+    for i in reversed(range(len(s))):
+        if s[i].startswith('0'):
+            cache[i] = 0
+        elif i == len(s) - 1:
+            cache[i] = 1
+        else:
+            if int(s[i:i + 2]) <= 26:
+                cache[i] = cache[i + 2]
+            cache[i] += cache[i + 1]
+    return cache[0]
+# Since each iteration takes O(1), the whole algorithm now takes O(n).
+
+output = num_encodings('11128775')
+
+print(str(output))
