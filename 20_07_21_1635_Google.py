fb1f474ee4cb56c1354eef661500010596fbdb29
Andre Doumad <andre@stringkeeper.com>
2020-08-01 22:57:14 -0700

    Andre Doumad

--- 20_07_21_1635_Google.py
+++ 20_07_21_1635_Google.py
@@ -119,3 +119,314 @@ class unitTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+'''
+OUTPUT:
+
+self.heap[0] : -9223372036854775807
+minHeapify index: 8
+minHeapify index: 7
+minHeapify index: 6
+minHeapify index: 5
+minHeapify index: 4
+minHeapify index: 3
+minHeapify index: 2
+minHeapify index: 1
+ parent: 1 left: 7 right: 3
+ parent: 7 left: 33 right: 57
+ parent: 3 left: 10 right: 5
+ parent: 33 left: 55 right: 99
+ parent: 57 left: 333 right: 666
+ parent: 10 left: 420 right: 200
+ parent: 5 left: 15 right: 11
+ parent: 55 left: 100 right: 0
+mHeap min is: 1
+mHeap min is: 3
+mHeap min is: 5
+mHeap min is: 7
+mHeap min is: 10
+mHeap min is: 11
+mHeap min is: 15
+mHeap min is: 33
+mHeap min is: 55
+mHeap min is: 57
+mHeap min is: 99
+mHeap min is: 100
+mHeap min is: 200
+mHeap is returning numbers larger than k
+mheap returned 420
+adding 1 + 3
+testing result: 4
+adding 1 + 5
+testing result: 6
+adding 1 + 7
+testing result: 8
+adding 1 + 10
+testing result: 11
+adding 1 + 11
+testing result: 12
+adding 1 + 15
+testing result: 16
+adding 1 + 33
+testing result: 34
+adding 1 + 55
+testing result: 56
+adding 1 + 57
+testing result: 58
+adding 1 + 99
+testing result: 100
+adding 1 + 100
+testing result: 101
+adding 1 + 200
+testing result: 201
+adding 3 + 1
+testing result: 4
+adding 3 + 5
+testing result: 8
+adding 3 + 7
+testing result: 10
+adding 3 + 10
+testing result: 13
+adding 3 + 11
+testing result: 14
+adding 3 + 15
+testing result: 18
+adding 3 + 33
+testing result: 36
+adding 3 + 55
+testing result: 58
+adding 3 + 57
+testing result: 60
+adding 3 + 99
+testing result: 102
+adding 3 + 100
+testing result: 103
+adding 3 + 200
+testing result: 203
+adding 5 + 1
+testing result: 6
+adding 5 + 3
+testing result: 8
+adding 5 + 7
+testing result: 12
+adding 5 + 10
+testing result: 15
+adding 5 + 11
+testing result: 16
+adding 5 + 15
+testing result: 20
+adding 5 + 33
+testing result: 38
+adding 5 + 55
+testing result: 60
+adding 5 + 57
+testing result: 62
+adding 5 + 99
+testing result: 104
+adding 5 + 100
+testing result: 105
+adding 5 + 200
+testing result: 205
+adding 7 + 1
+testing result: 8
+adding 7 + 3
+testing result: 10
+adding 7 + 5
+testing result: 12
+adding 7 + 10
+testing result: 17
+adding 7 + 11
+testing result: 18
+adding 7 + 15
+testing result: 22
+adding 7 + 33
+testing result: 40
+adding 7 + 55
+testing result: 62
+adding 7 + 57
+testing result: 64
+adding 7 + 99
+testing result: 106
+adding 7 + 100
+testing result: 107
+adding 7 + 200
+testing result: 207
+adding 10 + 1
+testing result: 11
+adding 10 + 3
+testing result: 13
+adding 10 + 5
+testing result: 15
+adding 10 + 7
+testing result: 17
+adding 10 + 11
+testing result: 21
+adding 10 + 15
+testing result: 25
+adding 10 + 33
+testing result: 43
+adding 10 + 55
+testing result: 65
+adding 10 + 57
+testing result: 67
+adding 10 + 99
+testing result: 109
+adding 10 + 100
+testing result: 110
+adding 10 + 200
+testing result: 210
+adding 11 + 1
+testing result: 12
+adding 11 + 3
+testing result: 14
+adding 11 + 5
+testing result: 16
+adding 11 + 7
+testing result: 18
+adding 11 + 10
+testing result: 21
+adding 11 + 15
+testing result: 26
+adding 11 + 33
+testing result: 44
+adding 11 + 55
+testing result: 66
+adding 11 + 57
+testing result: 68
+adding 11 + 99
+testing result: 110
+adding 11 + 100
+testing result: 111
+adding 11 + 200
+testing result: 211
+adding 15 + 1
+testing result: 16
+adding 15 + 3
+testing result: 18
+adding 15 + 5
+testing result: 20
+adding 15 + 7
+testing result: 22
+adding 15 + 10
+testing result: 25
+adding 15 + 11
+testing result: 26
+adding 15 + 33
+testing result: 48
+adding 15 + 55
+testing result: 70
+adding 15 + 57
+testing result: 72
+adding 15 + 99
+testing result: 114
+adding 15 + 100
+testing result: 115
+adding 15 + 200
+testing result: 215
+adding 33 + 1
+testing result: 34
+adding 33 + 3
+testing result: 36
+adding 33 + 5
+testing result: 38
+adding 33 + 7
+testing result: 40
+adding 33 + 10
+testing result: 43
+adding 33 + 11
+testing result: 44
+adding 33 + 15
+testing result: 48
+adding 33 + 55
+testing result: 88
+adding 33 + 57
+testing result: 90
+adding 33 + 99
+testing result: 132
+adding 33 + 100
+testing result: 133
+adding 33 + 200
+testing result: 233
+adding 55 + 1
+testing result: 56
+adding 55 + 3
+testing result: 58
+adding 55 + 5
+testing result: 60
+adding 55 + 7
+testing result: 62
+adding 55 + 10
+testing result: 65
+adding 55 + 11
+testing result: 66
+adding 55 + 15
+testing result: 70
+adding 55 + 33
+testing result: 88
+adding 55 + 57
+testing result: 112
+adding 55 + 99
+testing result: 154
+adding 55 + 100
+testing result: 155
+adding 55 + 200
+testing result: 255
+adding 57 + 1
+testing result: 58
+adding 57 + 3
+testing result: 60
+adding 57 + 5
+testing result: 62
+adding 57 + 7
+testing result: 64
+adding 57 + 10
+testing result: 67
+adding 57 + 11
+testing result: 68
+adding 57 + 15
+testing result: 72
+adding 57 + 33
+testing result: 90
+adding 57 + 55
+testing result: 112
+adding 57 + 99
+testing result: 156
+adding 57 + 100
+testing result: 157
+adding 57 + 200
+testing result: 257
+RESULT IS : True
+self.heap[0] : -9223372036854775807
+minHeapify index: 2
+minHeapify index: 1
+ parent: 3 left: 7 right: 10
+ parent: 7 left: 15 right: 0
+mHeap min is: 3
+mHeap min is: 15
+mHeap min is: 10
+mHeap min is: 7
+adding 3 + 15
+testing result: 18
+adding 3 + 10
+testing result: 13
+adding 3 + 7
+testing result: 10
+adding 15 + 3
+testing result: 18
+adding 15 + 10
+testing result: 25
+adding 15 + 7
+testing result: 22
+adding 10 + 3
+testing result: 13
+adding 10 + 15
+testing result: 25
+adding 10 + 7
+testing result: 17
+RESULT IS : True
+.
+----------------------------------------------------------------------
+Ran 1 test in 0.001s
+
+OK
+'''
