4d224fcac1c63d1ca073cd54886be48f9d56c249
Andre Doumad <andre@stringkeeper.com>
2020-08-03 23:45:07 -0700

    Andre Doumad

--- 20_08_03_Google.py
+++ 20_08_03_Google.py
@@ -1,3 +1,4 @@
+# Andre Doumad
 '''
 This problem was asked by Google.
 
@@ -5,18 +6,6 @@ The area of a circle is defined as πr^2.
 Estimate π to 3 decimal places using a Monte Carlo method.
 
 Hint: The basic equation of a circle is x2 + y2 = r2.
-
-The Algorithm
-1. Initialize circle_points, square_points and interval to 0.
-2. Generate random point x.
-3. Generate random point y.
-4. Calculate d = x*x + y*y.
-5. If d <= 1, increment circle_points.
-6. Increment square_points.
-7. Increment interval.
-8. If increment < NO_OF_ITERATIONS, repeat from 2.
-9. Calculate pi = 4*(circle_points/square_points).
-10. Terminate.
 '''
 import random
 
