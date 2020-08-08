089908264b1f6fd1166957979b2110c170fe2c63
Andre Doumad <andre@stringkeeper.com>
2020-08-08 11:29:34 -0700

    Andre Doumad

--- 20_08_08_Facebook.py
+++ 20_08_08_Facebook.py
@@ -10,29 +10,57 @@ cost to build the nth house with kth color, return the minimum cost which achiev
 '''
 import unittest
 
-
 class Solution(object):
-    
+    def __init__(self):
+        self.colors = ['red', 'green', 'blue', 'yellow']
+
+    def arrangeColors(self, houseCosts):
+        houseColors = {}
+        previousColorIndex = None
+        for k,v in houseCosts.items():
+            print('house[', k, ' color costs = ' + str(v))
+            cheapestColorPrice = max(v) + 1
+            cheapestIndex = 0
+            for i in range(0, len(v)):
+                if previousColorIndex == None and v[i] < cheapestColorPrice:
+                    cheapestColorPrice = v[i]
+                    cheapestIndex = i
+                elif previousColorIndex != None and v[i] < cheapestColorPrice and i != previousColorIndex:
+                    cheapestColorPrice = v[i]
+                    cheapestIndex = i
+            print('cheapest color is: ', self.colors[cheapestIndex], ' cost is ', cheapestColorPrice)
+            houseColors[k] = cheapestIndex
+            previousColorIndex = cheapestIndex
+        
+        previousColorIndex = None
+        previousKey = 0
+        for k,v in houseColors.items(): 
+            print('house ', k, ' is ', self.colors[v])
+            if previousColorIndex == None:
+                previousColorIndex = v
+            else:
+                if previousColorIndex == v:
+                    print('two of the same colors in a row!')
+                    # based on the previous house and this house, 
+                    # choose the next cheapest color between them
+                    exit()
+            previousColorIndex = v
+            previousKey = k
+        return houseColors
+
     def solve(self, houseMatrix=None):
         houseCosts = {}
+        
         # populate houseCosts dict
         for a in range(0, len(houseMatrix)):
             for b in range(0, len(houseMatrix[a])):
-                print('house[', a, '] color[', b, '] costs: ', houseMatrix[a][b])
-                if a not in houseCosts:
-                    houseCosts[a] = [houseMatrix[a][b]]
+                if b not in houseCosts:
+                    houseCosts[b] = [houseMatrix[a][b]]
                 else:
-                    houseMatrix[a].append([houseMatrix[a][b]])
-
-        for k,v in houseCosts:
-            # houseCosts[k] = sorted(v)
-            print('house[', k, ' color costs = ', v)
-
-        print(houseCosts)
-
-        houseAdj = {}
-        houseColors = {}
-        # based on cost and house adjacency matrix choose color
+                    houseCosts[b].append(houseMatrix[a][b])
+        
+        result = self.arrangeColors(houseCosts)
+        return result
 
 class UnitTest(unittest.TestCase):
     
@@ -40,10 +68,10 @@ class UnitTest(unittest.TestCase):
         solution = Solution()
 
         houseMatrix = [
-    # houses 1,2,3,4,5,6,7
+    # houses 0,1,2,3,4,5,6
             [3,7,2,4,6,5,2], # red
-            [9,3,3,8,4,7,5], # green
-            [1,2,5,3,1,3,9], # blue
+            [9,3,3,8,3,7,5], # green
+            [1,2,5,3,8,3,9], # blue
             [5,2,1,7,4,5,8]  # yellow
             ]
 
@@ -51,3 +79,28 @@ class UnitTest(unittest.TestCase):
 
 if __name__=='__main__':
     unittest.main()
+
+'''
+OUTPUT:
+house[ 0  color costs = [3, 9, 1, 5]
+cheapest color is:  blue  cost is  1
+house[ 1  color costs = [7, 3, 2, 2]
+cheapest color is:  yellow  cost is  2
+house[ 2  color costs = [2, 3, 5, 1]
+cheapest color is:  red  cost is  2
+house[ 3  color costs = [4, 8, 3, 7]
+cheapest color is:  blue  cost is  3
+house[ 4  color costs = [6, 3, 8, 4]
+cheapest color is:  green  cost is  3
+house[ 5  color costs = [5, 7, 3, 5]
+cheapest color is:  blue  cost is  3
+house[ 6  color costs = [2, 5, 9, 8]
+cheapest color is:  red  cost is  2
+house  0  is  blue
+house  1  is  yellow
+house  2  is  red
+house  3  is  blue
+house  4  is  green
+house  5  is  blue
+house  6  is  red
+'''
