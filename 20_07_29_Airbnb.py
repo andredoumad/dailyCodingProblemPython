1b26cf80f36eb7a0755328bc5f2d2d2d91efea7a
Andre Doumad <andre@stringkeeper.com>
2020-08-10 17:39:53 -0700

    Andre Doumad

--- 20_07_29_Airbnb.py
+++ 20_07_29_Airbnb.py
@@ -2,9 +2,11 @@
 '''
 This problem was asked by Airbnb.
 
-Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
+Given a list of integers, write a function that returns 
+the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
 
-For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+For example, [2, 4, 6, 2, 5] should return 13, 
+since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
 
 Follow-up: Can you do this in O(N) time and constant space?
 
@@ -15,31 +17,26 @@ import unittest
 class Solution(object):
 
     def solve(self, n):
-        print('--------------')
-        return self.solveIt(n, 0, [], False)
+        print('-------------')
+        print('input: ', n)
+        print('-------------')
+        if len(n) <= 2:
+            print('result: ')
+            return max(0, max(n))
 
-    def solveIt(self, n, total, sums, left):
-        print('total ', total)
-        print('sums ', sums)
-        lOfN = len(n)
-        while lOfN >= 2:
-            lOfN -= 1
-            if n[lOfN-1] < n[lOfN]:
-                sums.append(n[lOfN])
-                total+= n[lOfN]
-                n.pop()
-                n.pop()
-                return self.solveIt(n, total, sums, False)
-            else:
-                sums.append(n[lOfN-1])
-                total+= n[lOfN-1]
-                n.pop()
-                n.pop()
-                return self.solveIt(n, total, sums, True)
-        if len(n) == 1 and left == False:
-            total += n[0]
-        return total
+        max_excluding_last= max(0, n[0])
+        print('max_excluding_last ', max_excluding_last)
+        max_including_last = max(max_excluding_last, n[1])
+        print('max_including_last ', max_including_last)
+        print('n[2:] ', n[2:])
+        for num in n[2:]:
+            print(num)
+            prev_max_including_last = max_including_last
 
+            max_including_last = max(max_including_last, max_excluding_last + num)
+            max_excluding_last = prev_max_including_last
+        print('result: ')
+        return max(max_including_last, max_excluding_last)
 
 class unitTest(unittest.TestCase):
     def test_a(self):
@@ -55,39 +52,13 @@ if __name__ == '__main__':
 
 '''
 OUTPUT:
-
-
---------------
-total  0
-sums  []
-total  5
-sums  [5]
-total  11
-sums  [5, 6]
 13
---------------
-total  0
-sums  []
-total  5
-sums  [5]
-total  10
-sums  [5, 5]
 10
---------------
-total  0
-sums  []
-total  105
-sums  [105]
-total  147
-sums  [105, 42]
-total  147
-sums  [105, 42, 0]
-total  179
-sums  [105, 42, 0, 32]
-total  196
-sums  [105, 42, 0, 32, 17]
-total  197
-sums  [105, 42, 0, 32, 17, 1]
-197
+180
+.
+----------------------------------------------------------------------
+Ran 1 test in 0.000s
+
+OK
 
 '''
