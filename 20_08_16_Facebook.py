dd91de30b2891568e7a422dc4a5d9da1c23aaf04
Andre Doumad <andre@stringkeeper.com>
2020-08-16 10:56:56 -0700

    Andre Doumad

--- 20_08_16_Facebook.py
+++ 20_08_16_Facebook.py
@@ -9,3 +9,58 @@ For example, given the string "([])[]({})", you should return true.
 
 Given the string "([)]" or "((()", you should return false.
 '''
+import unittest
+class Solution:
+    def isValid(self, s):
+        brackMap= {'(':')','{':'}','[':']'}
+        lefBracks = set(['(','{','['])
+        stack = []
+        for val in s:
+            if val in lefBracks:
+                stack.append(val)
+            elif stack and val == brackMap[stack[-1]]:
+                stack.pop()
+            else:
+                return False
+        return stack == []
+
+
+class UnitTest(unittest.TestCase):
+    def test_a(self):
+        solution = Solution()
+
+        result = solution.isValid(r"()")
+        print('result is ', result)
+        self.assertEqual(result,True)
+
+        result = solution.isValid(r"()[]{}")
+        print('result is ', result)
+        self.assertEqual(result,True)
+
+        result = solution.isValid(r"(]")
+        print('result is ', result)
+        self.assertEqual(result,False)
+
+        result = solution.isValid(r"([)]")
+        print('result is ', result)
+        self.assertEqual(result,False)
+
+        result = solution.isValid(r"{[]}")
+        print('result is ', result)
+        self.assertEqual(result,True)
+
+if __name__ == '__main__':
+    unittest.main()
+
+'''
+result is  True
+result is  True
+result is  False
+result is  False
+result is  True
+.
+----------------------------------------------------------------------
+Ran 1 test in 0.000s
+
+OK
+'''
